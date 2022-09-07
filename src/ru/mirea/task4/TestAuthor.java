c4012bb64c8252bd53d1c9be3fc92b2d112fbefc
Fedor <shmakov987@mail.ru>
2022-09-07 18:52:32 +0300

    Задание 4 выполнено

--- src/ru/mirea/task4/TestAuthor.java
+++ src/ru/mirea/task4/TestAuthor.java
@@ -4,5 +4,7 @@ public class TestAuthor {
     public static void main(String[] args) {
         Author author = new Author("Fedor","fedor@gmail.com",'m');
         System.out.println(author.toString());
+        author.setEmail("123@gmail.com");
+        System.out.println(author.toString());
     }
 }
